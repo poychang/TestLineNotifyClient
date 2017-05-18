f0906bda80aa0ebf161487c08836be197fffa3c5
PoyChang <poypost@gmail.com>
2017-05-18 10:37:49 +0800

    利用 truthy and falsy 的方式改寫判斷式

--- src/app/share/app-config.service.ts
+++ src/app/share/app-config.service.ts
@@ -7,8 +7,7 @@ export class AppConfigService {
 
   /** 儲存 Line Notify 的 Token */
   public saveToken = (token: string) => {
-    if (token == undefined || null) return;
-    window.localStorage.setItem('token', token);
+    return token ? window.localStorage.setItem('token', token) : '';
   }
   /** 取得 Line Notify 的 Token */
   public getToken = () => { return window.localStorage.getItem('token'); }
